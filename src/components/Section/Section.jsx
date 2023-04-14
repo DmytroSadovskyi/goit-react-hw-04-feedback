6cb7141b3d89b37e3430a146001336adf9c79196
DmytroSadovskyi <111589023+DmytroSadovskyi@users.noreply.github.com>
2023-04-14 19:27:40 +0300

    Initial commit

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+import { FeedbackSection, Title } from './Section.styled';
+const Section = ({ title, children }) => {
+  return (
+    <FeedbackSection>
+      {title && <Title>{title}</Title>}
+      {children}
+    </FeedbackSection>
+  );
+};
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+export default Section;
